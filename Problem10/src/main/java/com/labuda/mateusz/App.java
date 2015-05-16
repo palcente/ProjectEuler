78770ef9beb2c040fca67b97137eec7472ce48a4
palcente <palcente@gmail.com>
2015-05-16 15:10:47 +0100

    aa

--- Problem10/src/main/java/com/labuda/mateusz/App.java
+++ Problem10/src/main/java/com/labuda/mateusz/App.java
@@ -1,30 +1,13 @@
 package com.labuda.mateusz;
 
-/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+import java.util.concurrent.ForkJoinPool;
+
 public class App {
 	
-public static void main(String[] args) {
-	int a=1;	
-	outer:
-	while(a<1000){
-		a++;
-		for(int b=1;b<999;b++){
-		int squareC = square(a)+square(b);
-		double c= Math.sqrt(squareC);
-		if(c%1==0 && a+b+c==1000){
-		int product =(int) (a*b*c);
-		System.out.println(product);
-		System.out.println("got it "+ a +" "+b+" "+c );
-		break outer;
-		}
-		}
-	}
-	
-}
+	  public static ForkJoinPool fjPool = new ForkJoinPool();
 
-	public static int square(int number){
-		return (int) Math.pow(number,2);
+	public static void main(String[] args) {
+		System.out.println(fjPool.invoke(new WorkerThread(2,2_000_001)).toString());
+
 	}
 }
