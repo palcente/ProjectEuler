df907bd598c05d3574a6db512a42a810e69830ad
matt <matt@matt-PC>
2015-05-31 03:34:47 +0100

    a

--- Problem13/src/main/java/com/labuda/mateusz/App.java
+++ Problem13/src/main/java/com/labuda/mateusz/App.java
@@ -1,28 +1,22 @@
 package com.labuda.mateusz;
 
-/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 */
 public class App {
-	static int target = 999;
-
-/*	public static void main(String[] args) {
-		int sum=0;
-		for(int n = 1; n<1000; n++){
-			if(n%3==0 || n%5==0){
-				sum+=n;
-			}
-		}
-		System.out.println(sum);
-	}*/
-	
 	public static void main(String[] args) {
-		System.out.println(sumDivisibleBy(3)+sumDivisibleBy(5)-sumDivisibleBy(15));
+		InputStream is = App.class.getResourceAsStream("/number");
+		Scanner s = new Scanner(is);
+
 	}
 	
-	public static int sumDivisibleBy(int number){
-		int p = target/number;
-		return number*(p*(p+1))/2;
+	static void decompose(Scanner s){
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		while(s.hasNext()){
+			s.nextLine().toCharArray();
+		}
 	}
-
 }
